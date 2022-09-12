010e7a660a55d20a577ab8d69e4f1bf1752873b3
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-09-12 12:58:17 -0700

    commented out unfinished app.hot.js

--- app-hot.js
+++ app-hot.js
@@ -1,6 +1,6 @@
-const tailsDesision = document.getElementById('tails');
-const headsDesision = document.getElementById('heads');
-const playAgain = document.getElementById('play-again');
+// const tailsDesision = document.getElementById('tails');
+// const headsDesision = document.getElementById('heads');
+// const playAgain = document.getElementById('play-again');
 
-const choiceContainer = document.getElementById('choice-container');
-const resultsContainer = document.getElementById('results-container');
+// const choiceContainer = document.getElementById('choice-container');
+// const resultsContainer = document.getElementById('results-container');
